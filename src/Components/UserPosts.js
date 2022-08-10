100395146afb19996b0c22b3c50bbed7b07e274f
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-08-10 18:46:37 -0300

    implement style in UserPage

--- src/Components/UserPosts.js
+++ src/Components/UserPosts.js
@@ -1,20 +1,91 @@
 import styled from "styled-components";
+import { AiOutlineHeart } from "react-icons/ai";
 
 export default function UserPosts({ user }) {
   const { posts } = user;
 
   return (
-    <>{posts.map((post, index) =>
-      <Post key={ index }>
-        <img src={ user.userPhoto } alt="" />
-        <h2>{ user.name }</h2>
-        <h2>{ post.content }</h2>
-        <a href={ post.postUrl }>{ post.postUrl }</a>
-      </Post>
-    )}</>
+    <Posts>
+      {posts.map((post, index) =>
+        <div className="post" key={ index }>
+          <div className="left-side">
+            <img src={ user.userPhoto } alt="" />
+            <AiOutlineHeart size={ "20px" } />
+            <h3>X Likes</h3> {/* Colocar número de likes */}
+          </div>
+          <div className="right-side">
+            <h2>{ user.name }</h2>
+            <h2>{ post.content }</h2>
+            <a href={ post.postUrl }>{ post.postUrl }</a>
+          </div>
+        </div>
+      )}
+    </Posts>
   );
 }
 
-const Post = styled.div`
+const Posts = styled.div`
+  font-family: "Lato";
+  color: #FFFFFF;
 
+  width: 100%;
+  margin-top: 48px;
+
+  display: flex;
+  flex-direction: column;
+
+  a {
+    text-decoration: none;
+    color: #FFFFFF;
+  }
+
+  .post {
+    background-color: #171717;
+    border-radius: 16px;
+
+    width: 100%;
+    padding: 18px;
+    margin-bottom: 18px;
+
+    display: flex;
+  }
+
+  .left-side {
+    width: 10%;
+    margin-right: 3%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    img {
+      margin-bottom: 15px;
+    }
+
+    h3 {
+      font-size: 11px;
+      font-weight: 400;
+
+      margin-top: 3px;
+    }
+  }
+
+  .right-side {
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+
+    h2:first-child {
+      font-size: 19px;
+      font-weight: 400;
+    }
+
+    h2:nth-child(2) {
+      color: #B7B7B7;
+      font-size: 17px;
+      font-weight: 400;
+
+      margin: 7px 0;
+    }
+  }
 `;
--- src/MainRoutes/UserPage.js
+++ src/MainRoutes/UserPage.js
@@ -21,14 +21,60 @@ export default function UserPage() {
 
   return (
     <Main>
-      <img src={ user.userPhoto } alt="" />
-      <h1>{ user.name }'s posts</h1>
-      <UserPosts user={ user } />
-      {/* <Trendings /> */}
+      <div className="container">
+        <div className="left-side">
+          <div className="top">
+            <img src={ user.userPhoto } alt="" />
+            <h1>{ user.name }'s posts</h1>
+          </div>
+          <UserPosts user={ user } />
+        </div>
+        <div className="right-side">
+          {/* <Trendings /> */}
+        </div>
+      </div>
     </Main>
   );
 }
 
 const Main = styled.main`
+  background-color: #333333;
 
+  width: 100%;
+  height: 100vh;
+
+  .container {
+    width: 90%;
+    padding-top: 132px;
+
+    display: flex;
+    justify-content: center;
+  }
+
+  .left-side {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    img {
+      border-radius: 50%;
+      object-fit: cover;
+
+      width: 50px;
+      height: 50px;
+    }
+
+    .top {
+      margin-left: 18px;
+      display: flex;
+
+      h1 {
+        color: #FFFFFF;
+        font-family: "Oswald";
+        font-size: 43px;
+        font-weight: 700;
+        margin-left: 18px;
+      }
+    }
+  }
 `;
